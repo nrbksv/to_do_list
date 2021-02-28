936f26651a3205e9767d6f2466c106b68852dac6
nrbk <nrbksv@gmail.com>
2021-02-28 12:05:12 +0600

    Migrations made.

--- to_do_list/task/models.py
+++ to_do_list/task/models.py
@@ -5,7 +5,7 @@ class Task(models.Model):
     STATUS_CHOICES = [('new', 'Новая'), ('in_progress', 'В процессе'), ('done', 'Сделано')]
 
     status = models.CharField(max_length=20, default='new', blank=True)
-    task_title = models.CharField(max_length=200, null=False, blank=False)
+    task_title = models.CharField(max_length=200, default='', null=False, blank=False)
     full_description = models.TextField(max_length=2000, null=True, blank=True)
     deadline = models.DateField(blank=True, null=True)
 
